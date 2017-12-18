15b7a45f1e0020589be22f5daf8d9610363e363a
Gareth Jones <gareth.jones@shinetech.com>
2017-12-18 22:00:49 +1100

    made a dog. sort of.

---
+++ Assets/Dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("space");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("rotate left");
+        } 
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("rotate right");  
+        }
+    }
+}
