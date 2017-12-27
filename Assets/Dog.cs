28ea9b83442826b252dba956fca1f56a59893acb
Gareth Jones <gareth.jones@shinetech.com>
2017-12-27 18:47:16 +1100

    dog farts while thrusting

--- Assets/Dog.cs
+++ Assets/Dog.cs
@@ -6,30 +6,46 @@ using UnityEngine;
 public class Dog : MonoBehaviour {
 
     Rigidbody rigidbody;
+    AudioSource audioSource;
+    bool farting = false;
 
 	// Use this for initialization
 	void Start () {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         ProcessInput();
+        CheckSounds();
 	}
+
+    private void CheckSounds() {
+        if (farting && !audioSource.isPlaying) {
+            audioSource.Play();
+        } else if (!farting) {
+            audioSource.Stop();
+        }
+    }
 
     private void ProcessInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidbody.AddRelativeForce(Vector3.up);
+            farting = true;
+        } else
+        {
+            farting = false;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rigidbody.AddTorque(Vector3.forward);
+            transform.Rotate(Vector3.forward);
         } 
         else if (Input.GetKey(KeyCode.D))
         {
-            rigidbody.AddTorque(-Vector3.forward);
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
