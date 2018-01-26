d26add6de50a1b9d86885d1681a0707044c39753
Gareth Jones <gareth.jones@shinetech.com>
2018-01-26 11:48:38 +1100

    added more levels, and a rotator script

---
+++ Assets/Rotator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    [SerializeField]
+    Vector3 rotationPoint = new Vector3(0, 0, 0);
+    [SerializeField]
+    float speed = 1.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationThisFrame = speed * Time.deltaTime;
+        transform.RotateAround(rotationPoint, Vector3.forward, rotationThisFrame);
+    }
+}
